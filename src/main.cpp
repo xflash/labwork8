8bb0209fa19ebd198cedcf67c0b5a07f0261d355
Rémy Coqueugniot <rcoqueugniot@gmail.com>
2019-05-16 22:33:04 +0200

    Usage of vector instead of list

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 #include <iostream>
 #include <string>
 #include <list>
@@ -9,17 +11,19 @@ using namespace std;
 
 #include "Station.h"
 
-Station *findStation(int id);
+/////////////////////////////
 
-void readStations(const char *filename, std::list<Station *> *list);
+Station *findStation(std::vector<Station *> *stations, int id);
+
+void readStations(const char *filename, std::vector<Station *> *stations);
 
 Station *createStation(int id, string name);
 
 void displayStation(Station *item);
 
-void displayList(std::list<Station *> *list);
+void displayList(std::vector<Station *> *stations);
 
-void cleanupList(std::list<Station *> *list);
+void cleanupStations(std::vector<Station *> *stations);
 
 /////////////////////////////
 
@@ -37,22 +41,24 @@ vector<string> split(const char *str, char c = ';') {
     return result;
 }
 
-Station *findStation(std::list<Station *> *list, int id) {
-    for (const auto &item : *list) {
+Station *findStation(std::vector<Station *> *stations, int id) {
+    for (const auto &item : *stations) {
         if (item->id == id)
             return item;
     }
+
+    cerr << "Station id " << id << " not found " << endl;
 
     return nullptr;
 }
 
 
-void readStations(const char *filename, std::list<Station *> *list) {
+void readStations(const char *filename, std::vector<Station *> *stations) {
     std::ifstream stations_file(filename);
     std::string line;
     while (std::getline(stations_file, line)) {
         const vector<string> &cells = split(line.data());
-        list->push_back(createStation(
+        stations->push_back(createStation(
                 std::stoi(cells.at(1)),
                 cells.at(0)));
     }
@@ -61,30 +67,31 @@ void readStations(const char *filename, std::list<Station *> *list) {
 Station *createStation(int id, string name) {
     Station *pStation = new Station;
     pStation->id = id;
-    pStation->name = name;
+    pStation->name = std::move(name);
     return pStation;
 }
 
-void displayStation(Station *item) { cout << item->id << ":" << item->name << endl; }
+void displayStation(Station *item) {
+    cout << item->id << ":" << item->name << endl;
+}
 
-void displayList(std::list<Station *> *list) {
-    for (const auto &item : *list) {
+void displayList(std::vector<Station *> *stations) {
+    for (const auto &item : *stations) {
         displayStation(item);
     }
     cout << endl;
 }
 
-void cleanupList(std::list<Station *> *list) {
-    cout << "Cleaning up list of " << list->size() << " elements" << endl;
-
-    for (auto &item : *list) {
+void cleanupStations(std::vector<Station *> *stations) {
+    cout << "Cleaning up " << stations->size() << " stations" << endl;
+    for (auto &item : *stations) {
         delete item;
     }
 }
 
 int main() {
 
-    std::list<Station *> stations = std::list<Station *>();
+    std::vector<Station *> stations = std::vector<Station *>();
 
     readStations("data/stations.csv", &stations);
 
@@ -94,10 +101,10 @@ int main() {
     Station *station = findStation(&stations, id);
     if (station != nullptr) {
         cout << "Station id " << id << " found : " << station->name << endl;
-    } else {
-        cerr << "Station not found " << endl;
     }
+    // 99999 should not be founded
+    findStation(&stations, 99999);
 
 
-    cleanupList(&stations);
+    cleanupStations(&stations);
 }
